2ea6dbf1915b521afd4d860c56d3a32ec756ac41
Neitaddi <naitaddimariem@gmail.com>
2023-12-01 16:33:39 +0000

    acceuil

---
+++ acceuil.js
@@ -0,0 +1,20 @@
+console.log("hello kawther");
+
+const cours = {
+  nom: "javascript",
+  salle: "dakar",
+  nbr_detudient: 2,
+  date: "1-12-2023",
+};
+
+console.table(cours);
+
+function somme(x, y) {
+  var s = 0;
+  //   s = x + y;
+
+  //   return s;
+  console.log("s", s);
+}
+
+console.log("somme", somme(1, 3));
